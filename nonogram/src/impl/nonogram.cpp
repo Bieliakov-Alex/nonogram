9d181f2dedb45308ada52b43925433717bf4cf3c
Alex Bieliakov <bieliakov.alex@gmail.com>
2020-02-28 18:34:39 +0200

    Add out of bounds error check in getRowFigures/getColumnfigures impl
    This fix prevents runtime crash after requesting not existing row/column
    figures.

--- nonogram/src/impl/nonogram.cpp
+++ nonogram/src/impl/nonogram.cpp
@@ -1,8 +1,11 @@
 /*Copyright 2020 Alex Bieliakov*/
 
-#include <boost/log/trivial.hpp>
-
 #include "nonogram.hpp"
+
+#include <sstream>
+#include <stdexcept>
+
+#include <boost/log/trivial.hpp>
 
 Nonogram::Nonogram(const std::vector<std::vector<uint8_t>> &RowsFigures,
                    const std::vector<std::vector<uint8_t>> &ColumnsFigures)
@@ -22,12 +25,39 @@ uint8_t Nonogram::getColumns() {
 }
 
 std::vector<uint8_t> Nonogram::getRowFigures(uint8_t row) {
-  BOOST_LOG_TRIVIAL(info) << "Nonogram getRowFigures calling " << row << ".";
-  return rows_figures_[row];
+  BOOST_LOG_TRIVIAL(info) << "Nonogram getRowFigures calling "
+                          << static_cast<unsigned int>(row) << ".";
+  if (row >= rows_) {
+    BOOST_LOG_TRIVIAL(error) << "Error in getRowFigures. Value of row ("
+                             << static_cast<unsigned int>(row) << ")"
+                             << " is biger or equal than value of rows_ ("
+                             << static_cast<unsigned int>(rows_) << ").";
+    std::stringstream error_stream;
+    error_stream << "Error in getRowFigures. Value of row ("
+                 << static_cast<unsigned int>(row) << ")"
+                 << " is biger or equal than value of rows_ ("
+                 << static_cast<unsigned int>(rows_) << ").";
+    throw std::runtime_error(error_stream.str());
+  } else {
+    return rows_figures_[row];
+  }
 }
 
 std::vector<uint8_t> Nonogram::getColumnFigures(uint8_t column) {
-  BOOST_LOG_TRIVIAL(info) << "Nonogram getColumnFigures calling " << column
-                          << ".";
-  return column_figures_[column];
+  BOOST_LOG_TRIVIAL(info) << "Nonogram getColumnFigures calling "
+                          << static_cast<unsigned int>(column) << ".";
+  if (column >= columns_) {
+    BOOST_LOG_TRIVIAL(error) << "Error in getColumnFigures. Value of row ("
+                             << static_cast<unsigned int>(column) << ")"
+                             << " is biger or equal than value of rows_ ("
+                             << static_cast<unsigned int>(columns_) << ").";
+    std::stringstream error_stream;
+    error_stream << "Error in getColumnFigures. Value of row ("
+                 << static_cast<unsigned int>(column) << ")"
+                 << " is biger or equal than value of rows_ ("
+                 << static_cast<unsigned int>(columns_) << ").";
+    throw std::runtime_error(error_stream.str());
+  } else {
+    return column_figures_[column];
+  }
 }
